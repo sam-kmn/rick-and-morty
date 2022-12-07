7f5206d853e2522f92d7f39d6a5bbe98b6e08493
Samuel <91400290+sam-kmn@users.noreply.github.com>
2022-12-07 22:54:17 +0100

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import SelectSpecies from './components/SelectSpecies'
 import Pagination from './components/Pagination'
 import SearchBar from './components/SearchBar'
-import SelectSpecies from './components/SelectSpecies'
 import Table from './components/Table'
 
 function App() {
   return (
-    <main className="w-full max-w-6xl mx-auto h-full flex flex-col gap-5 justify-center ">
-      <h1 className="font-semibold text-xl w-full text-start">Characters</h1>
+    <main className="w-full max-w-6xl mx-auto h-full flex flex-col py-20  justify-between ">
+      <div className="w-full flex flex-col gap-5 ">
+        <h1 className="font-semibold text-2xl w-full text-start">Characters</h1>
 
-      <div className="flex  max-w-sm gap-10">
-        <SearchBar />
-        <SelectSpecies />
+        <div className="flex  max-w-sm gap-10">
+          <SearchBar />
+          <SelectSpecies />
+        </div>
+
+        <Table />
       </div>
-      <Table />
+
       <Pagination />
     </main>
   )
